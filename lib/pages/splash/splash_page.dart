a6e87181ae724d3b4b5d0311a23dab1c87b69818
flpcovre PC <filipecovre224@gmail.com>
2025-05-13 18:44:33 -0300

    Implementando animação de saída, e simulando uma autenticação.

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -13,9 +13,15 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
   late final Animation<double> _imageScale;
 
   late final AnimationController _textController;
+  late final AnimationController _exitController;
+  late final Animation<double> _fadeOutAnimation;
+  late final Animation<Offset> _slideOutAnimation;
 
   String fullText = 'Si\$Money';
   int currentLength = 0;
+
+  bool _animationCompleted = false;
+  bool _authCompleted = false;
 
   @override
   void initState() {
@@ -37,20 +43,78 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
       vsync: this,
       duration: Duration(milliseconds: 50 * fullText.length),
     )..addListener(() {
-        setState(() {
-          currentLength = (_textController.value * fullText.length).floor();
-        });
+      setState(() {
+        currentLength = (_textController.value * fullText.length).floor();
       });
+
+      if (_textController.isCompleted) {
+        _onAnimationComplete();
+      }
+    });
 
     Future.delayed(Duration(seconds: 1), () {
       _textController.forward();
     });
+
+    _simulateAuth();
+
+    _exitController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+    );
+
+    _fadeOutAnimation = CurvedAnimation(
+      parent: _exitController,
+      curve: Curves.easeOut,
+    );
+
+    _slideOutAnimation = Tween<Offset>(
+      begin: Offset.zero,
+      end: Offset(0.0, -1.0),
+    ).animate(
+      CurvedAnimation(parent: _exitController, curve: Curves.easeInOut),
+    );
+  }
+
+  Future<void> _simulateAuth() async {
+    await Future.delayed(Duration(seconds: 2));
+    _onAuthComplete();
+  }
+
+  void _onAnimationComplete() {
+    _animationCompleted = true;
+    _checkIfReadyToExit();
+  }
+
+  void _onAuthComplete() {
+    _authCompleted = true;
+    _checkIfReadyToExit();
+  }
+
+  Future<void> _checkIfReadyToExit() async {
+    if (_animationCompleted && _authCompleted) {
+      await Future.delayed(Duration(seconds: 1));
+      _startExitAnimation();
+    }
+  }
+
+  void _startExitAnimation() {
+    _exitController.forward().whenComplete(() {
+      _goToNextPage();
+    });
+  }
+
+  void _goToNextPage() {
+    Navigator.of(
+      context,
+    ).pushReplacement(MaterialPageRoute(builder: (_) => const Placeholder()));
   }
 
   @override
   void dispose() {
     _imageController.dispose();
     _textController.dispose();
+    _exitController.dispose();
     super.dispose();
   }
 
@@ -61,7 +125,7 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
       return TextSpan(
         text: char,
         style: TextStyle(
-          fontSize: 36,
+          fontSize: 40,
           fontWeight: FontWeight.bold,
           color: color,
           letterSpacing: 6.0,
@@ -81,24 +145,27 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return GradientScaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ScaleTransition(
-              scale: _imageScale,
-              child: Image.asset('lib/assets/img/robot.png', width: 100),
-            ),
-            const SizedBox(height: 16),
-
-            SizedBox(
-              height: 50,
-              child: RichText(
-                text: TextSpan(
-                  children: _buildStyledText(fullText),
+        child: FadeTransition(
+          opacity: ReverseAnimation(_fadeOutAnimation),
+          child: SlideTransition(
+            position: _slideOutAnimation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ScaleTransition(
+                  scale: _imageScale,
+                  child: Image.asset('lib/assets/img/robot.png', width: 100),
                 ),
-              ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  height: 50,
+                  child: RichText(
+                    text: TextSpan(children: _buildStyledText(fullText)),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
